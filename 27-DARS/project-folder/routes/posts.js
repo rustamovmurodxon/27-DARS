92901ca3e9589c09618ec3b8580bf2bf9501e28b
rustamov murodxon <rmurodxon18@gmail.com>
2025-03-25 15:38:07 +0500

    Update posts.js

--- 27-DARS/project-folder/routes/posts.js
+++ 27-DARS/project-folder/routes/posts.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { readData, writeData } = require('../dataHandler');
 
-// Barcha postlarni olish
 router.get('/', (req, res) => {
     const data = readData();
     res.json(data.posts);
 });
 
-// Yangi post qo'shish
 router.post('/', (req, res) => {
     const data = readData();
     const newPost = req.body;
