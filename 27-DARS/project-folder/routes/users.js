a50da03656c1a6c9ff26b4ebedafd97d9421865b
rustamov murodxon <rmurodxon18@gmail.com>
2025-03-25 15:38:31 +0500

    Update users.js

--- 27-DARS/project-folder/routes/users.js
+++ 27-DARS/project-folder/routes/users.js
@@ -2,13 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { readData, writeData } = require('../dataHandler');
 
-// Barcha foydalanuvchilarni olish
 router.get('/', (req, res) => {
     const data = readData();
     res.json(data.users);
 });
 
-// Foydalanuvchi qo'shish
 router.post('/', (req, res) => {
     const data = readData();
     const newUser = req.body;
